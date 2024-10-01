7ac952a610c6832c69e06698365d4fac7d00428b
George-25 <georgemallory26@gmail.com>
2024-10-01 12:00:40 -0400

    middle-ware final stage

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 
 const dotenv = require('dotenv');
 dotenv.config();
+
 const express = require('express');
 const app = express();
+
 const mongoose = require('mongoose');
+
 const methodOverride = require('method-override');
+
 const morgan = require('morgan');
+
 const session = require('express-session');
+
+// require our new middleware!
+const isSignedIn = require('./middleware/is-signed-in.js');
+const passUserToView = require('./middleware/pass-user-to-view.js');
+
 
 const authController = require('./controllers/auth.js');
 
@@ -28,6 +38,15 @@ app.use(
     saveUninitialized: true,
   })
 );
+//////////
+app.use(passUserToView); // use new passUserToView middleware here
+
+app.get('/', (req, res) => {
+  res.render('index.ejs', {
+    user: req.session.user,
+  });
+});
+//////////////
 
 app.get('/', (req, res) => {
   res.render('index.ejs', {
@@ -37,6 +56,8 @@ app.get('/', (req, res) => {
 
 
 app.use('/auth', authController);
+app.use(isSignedIn); // use new isSignedIn middleware here
+
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
